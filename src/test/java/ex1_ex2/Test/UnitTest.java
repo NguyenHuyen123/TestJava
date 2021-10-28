62fc51079b541c18432e783e8f46e58a1d9e3d66
Huyen-ntt <nguyenhuyenpy98@gmail.com>
2021-10-28 18:57:20 +0700

    Test Java

--- src/test/java/ex1_ex2/Test/UnitTest.java
+++ src/test/java/ex1_ex2/Test/UnitTest.java
@@ -37,23 +37,47 @@ public class UnitTest {
         double expect = 3.0;
         Assertions.assertEquals(expect, tb);
     }
+
     @Test
-    @DisplayName("Is Email")// Là email
+    @DisplayName("Is Email")
+// Là email
     void isEmail() {
 
         Student actual = new Student(18424005, "Đặng Văn Thường", "dvthuong@gmail.com", 0, 0, 0, 7.2);
-        boolean tb = actual.isVailEmail( );
+        boolean tb = actual.isVailEmail();
         boolean expect = true;
         Assertions.assertEquals(expect, tb);
     }
+
     @Test
-    @DisplayName("Is not Email")// không phải email
+    @DisplayName("Is not Email")
+// không phải email
     void isNotEmail() {
 
         Student actual = new Student(18424005, "Đặng Văn Thường", "dvthuong", 0, 0, 0, 7.2);
-        boolean tb = actual.isVailEmail( );
+        boolean tb = actual.isVailEmail();
         boolean expect = false;
         Assertions.assertEquals(expect, tb);
     }
 
+    @Test
+    @DisplayName("Is Gmail")
+// Là Gmail
+    void isGmail() {
+
+        Student actual = new Student(18424005, "Đặng Văn Thường", "dvthuong@gmail.com", 0, 0, 0, 7.2);
+        boolean tb = actual.isVailEmailGmail();
+        boolean expect = true;
+        Assertions.assertEquals(expect, tb);
+    }
+    @Test
+    @DisplayName("Is Not Gmail")
+// Là Gmail
+    void isNotGmail() {
+
+        Student actual = new Student(18424005, "Đặng Văn Thường", "dvthuong@ghcmuaf.com", 0, 0, 0, 7.2);
+        boolean tb = actual.isVailEmailGmail();
+        boolean expect = false;
+        Assertions.assertEquals(expect, tb);
+    }
 }
